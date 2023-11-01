339ff3b59733d9478633af79d287de15607a215a
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-01 19:14:39 +0200

    routes page for all events is created, all events data is being fetched, route successfully gets the data from drupal

---
+++ next/pages/events/index.tsx
@@ -0,0 +1,71 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { DrupalNode } from "next-drupal";
+import { useTranslation } from "next-i18next";
+
+import { ArticleTeasers } from "@/components/article-teasers";
+import { ContactForm } from "@/components/contact-form";
+import { ContactList } from "@/components/contact-list";
+import { LayoutProps } from "@/components/layout";
+import { LogoStrip } from "@/components/logo-strip";
+import { Meta } from "@/components/meta";
+import { Paragraph } from "@/components/paragraph";
+import { drupal } from "@/lib/drupal/drupal-client";
+import { getNodePageJsonApiParams } from "@/lib/drupal/get-node-page-json-api-params";
+import { getCommonPageProps } from "@/lib/get-common-page-props";
+import {
+  ArticleTeaser,
+  validateAndCleanupArticleTeaser,
+} from "@/lib/zod/article-teaser";
+import { Frontpage, validateAndCleanupFrontpage } from "@/lib/zod/frontpage";
+
+import { Divider } from "@/ui/divider";
+import { EventsCards } from "@/components/events/events-cards";
+
+interface IndexPageProps extends LayoutProps {
+  promotedArticleTeasers: ArticleTeaser[];
+}
+
+export default function Events({ events }) {
+  const { t } = useTranslation();
+
+  console.log(events);
+  return (
+    <>
+      <h1>Hello from events</h1>
+      {/* <Meta title={frontpage?.title} metatags={frontpage?.metatag} /> */}
+
+      {/* <EventsCards
+        articles={promotedArticleTeasers}
+        heading={t("promoted-articles")}
+      /> */}
+      {/* <ContactList /> */}
+      {/* <LogoStrip /> */}
+    </>
+  );
+}
+
+export const getStaticProps = async (context) => {
+  const events = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--events",
+    context,
+    //   {
+    // params: {
+    //   "filter[status]": 1,
+    //   "filter[langcode]": context.locale,
+    //   "filter[promote]": 1,
+    //   "fields[node--article]": "title,path,field_image,uid,created",
+    //   include: "field_image,uid",
+    //   sort: "-sticky,-created",
+    //   "page[limit]": 3,
+    // },
+    //   }
+  );
+
+  return {
+    props: {
+      ...(await getCommonPageProps(context)),
+      events: events,
+    },
+    revalidate: 60,
+  };
+};
