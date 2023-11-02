9654fb0bbf8aa832bc7aeee1790611f87dc6b0d0
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-03 01:50:31 +0200

    modified icon import strategy and css improvement

--- next/components/events/events-card.tsx
+++ next/components/events/events-card.tsx
@@ -5,12 +5,13 @@ import { useTranslation } from "next-i18next";
 import { absoluteUrl } from "@/lib/drupal/absolute-url";
 import { formatDate } from "@/lib/utils";
 import { ArticleTeaser } from "@/lib/zod/article-teaser";
-
+import CalenderIcon from "@/styles/icons/calendar.svg";
+import BuildingIcon from "@/styles/icons/building.svg";
 interface ArticleTeaserProps {
   article: ArticleTeaser;
 }
 
-export function EventCard({ event }: ArticleTeaserProps) {
+export function EventCard({ event }) {
   const { t } = useTranslation();
   const router = useRouter();
   const date = formatDate(event.field_start_date, router.locale);
@@ -18,20 +19,7 @@ export function EventCard({ event }: ArticleTeaserProps) {
     event.field_organizers.length > 0
       ? event.field_organizers.map((organizer) => (
           <li key={organizer.id} className="flex item-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="blue"
-              className="w-7 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
-              />
-            </svg>
+            <BuildingIcon className="h-6 w-6 text-primary-400" />
             <span>{organizer.field_organizer_name}</span>
           </li>
         ))
@@ -54,28 +42,14 @@ export function EventCard({ event }: ArticleTeaserProps) {
         {event.title}
       </h3>
       <div className="grid gird-col-1 p-4">
-        <div className="py-2 flex items-center gap-x-2 absolute top-2 left-2 bg-primary-200 px-2  rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="blue"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-            />
-          </svg>
-
+        <div className="py-2 flex items-center gap-x-2 absolute top-2 left-2 bg-graysuit px-2  rounded-full">
+          <CalenderIcon className="h-6 w-6 text-primary-400" />
           <time>{date}</time>
         </div>
         {/* some event might not have organizers */}
         {organizers && <ul className="mb-4">{organizers}</ul>}
         <p>{event.field_excerpt}</p>
-        <button className="border py-3 bg-primary-100 rounded-full mt-4 hover:bg-primary-400  text-center">
+        <button className="content-end border h-14 py-3 bg-primary-100 rounded-full mt-4 hover:bg-primary-400  text-center">
           Lear more
         </button>
       </div>
--- next/components/events/events-cards.tsx
+++ next/components/events/events-cards.tsx
@@ -13,11 +13,11 @@ interface LatestArticlesProps {
   heading: string;
 }
 
-export function EventsCards({ events, heading }: LatestArticlesProps) {
+export function EventsCards({ events, heading }) {
   const { t } = useTranslation();
   return (
-    <>
-      <h2 className="text-heading-sm font-bold md:text-heading-md">
+    <div className="grid justify-center">
+      <h2 className="pb-6 md:pb-8 text-center text-heading-sm font-bold md:text-heading-md">
         {heading}
       </h2>
       <ul className="mt-4 grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
@@ -27,6 +27,6 @@ export function EventsCards({ events, heading }: LatestArticlesProps) {
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
