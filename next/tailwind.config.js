5a9174e8e446bb8b129a9f56493c14db05e503af
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-11-07 11:12:26 +0200

    use tailwind for native share button

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./{components,lib,pages,stories,styles,ui}/**/*"],
+ 
   theme: {
     colors: {
       transparent: "transparent",
