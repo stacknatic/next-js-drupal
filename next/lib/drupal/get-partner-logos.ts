e892bb909c769cdd8b3ec635c94be62a28bdf26d
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-13 19:21:09 +0200

    getValidatedPartnerLogos() function is written which takes context as parameter and return validated and cleaned up object which contains list of partners logo and links. this function fetch partners_logos content type from drupal

---
+++ next/lib/drupal/get-partner-logos.ts
@@ -0,0 +1,23 @@
+import { DrupalNode } from "next-drupal";
+import { drupal } from "./drupal-client";
+import { logosLinksValidationCleanup } from "../zod/customer-partners-logos";
+
+export async function getValidatedPartnerLogos(context) {
+  // fetching data for partner_logos content type, there should be only one content of that type.
+  const logos = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--partners_logos",
+    context,
+    {
+      params: {
+        "filter[status]": 1,
+        "fields[node--partners_logos]": "title,field_logos",
+        include: "field_logos.field_logo",
+      },
+    },
+  );
+  // only first element of logos will se sent for clean up and validation and used in components
+  const logo = logos[0];
+  // data will be valideated in two leves and returns the completed object.
+  const validatedLogos = logosLinksValidationCleanup(logo);
+  return validatedLogos;
+}
