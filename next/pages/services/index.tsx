b23442a35a91e0120347a3e2281b284e09a0d3bf
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-29 21:59:40 +0200

    services and serviceCategories data is fetched and passed into ServicePage component

--- next/pages/services/index.tsx
+++ next/pages/services/index.tsx
@@ -2,19 +2,35 @@ import React from "react";
 import { GetStaticProps } from "next";
 import { getValidatedCleanServices } from "@/lib/drupal/get-services";
 import { getCommonPageProps } from "@/lib/get-common-page-props";
+import { getValidatedCleanServiceCategories } from "@/lib/drupal/get-service-catagories";
+import ServicePage from "@/components/services/service-page";
+import { ServiceDataType } from "@/lib/zod/services";
+import { ServiceCategoryType } from "@/lib/zod/service-categories";
 
-export default function Services({ services }) {
-  console.log(services)
-  return <div>Services</div>;
+type ServicesPropType = {
+  services: ServiceDataType[];
+  serviceCategories: ServiceCategoryType[];
+};
+
+export default function Services({
+  services,
+  serviceCategories,
+}: ServicesPropType) {
+  return (
+    <ServicePage services={services} serviceCategories={serviceCategories} />
+  );
 }
 
-export const getStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   try {
     const validatedServices = await getValidatedCleanServices(context);
+    const validatedServiceCategories =
+      await getValidatedCleanServiceCategories(context);
     return {
       props: {
         ...(await getCommonPageProps(context)),
         services: validatedServices,
+        serviceCategories: validatedServiceCategories,
       },
     };
   } catch (error) {
