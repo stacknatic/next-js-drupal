47908b812bbc69defd7fd785f344271930cff0d1
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-29 13:02:01 +0200

    Data fetching logic improvement
    Added some filter criteria only to fetch data we will need in front end. validatedCleanData function is imported and used to validate and clean up services data

--- next/lib/drupal/get-services.ts
+++ next/lib/drupal/get-services.ts
@@ -1,19 +1,24 @@
 import { DrupalNode } from "next-drupal";
 import { drupal } from "./drupal-client";
+import { validateCleanupServiceData } from "../zod/services";
 
 export const getValidatedCleanServices = async (context: any) => {
   const services = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
-    "node--services",
+    "node--service",
     context,
     {
       params: {
         "filter[status]": 1,
         "filter[langcode]": context.locale,
-        "fields[node--services]": "id,title,path,",
-        // include: "field_image,field_organizers",
+        "fields[node--service]":
+          "id,title,path,field_service_description,field_service_list,field_service_name,field_service_statement,field_service_category,field_service_image",
+        "fields[node--service_category]": "field_category_name",
+        "fields[file--file]": "uri",
+        include: "field_service_category,field_service_image",
       },
     },
   );
-
-  return services;
+  // validation and clean up of fetched data
+  const validatedCleanData = validateCleanupServiceData(services);
+  return validatedCleanData;
 };
