3ca73542921a2792e3c6d28190b5e084dee8b7cd
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-30 22:18:05 +0200

    Dynamic route for individual service
    data fetched for individual service route, validated with zod schema, received as props and component and added props type

---
+++ next/pages/services/[...slug].tsx
@@ -0,0 +1,79 @@
+import {
+  LanguageLinks,
+  createLanguageLinks,
+} from "@/lib/contexts/language-links-context";
+import { drupal } from "@/lib/drupal/drupal-client";
+import { getNodePageJsonApiParams } from "@/lib/drupal/get-node-page-json-api-params";
+import { getNodeTranslatedVersions } from "@/lib/drupal/get-node-translated-versions";
+import { getCommonPageProps } from "@/lib/get-common-page-props";
+import { ServiceDataType, ServiceSchema } from "@/lib/zod/services";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { DrupalNode } from "next-drupal";
+import React from "react";
+
+type ServicePageProps = {
+  service: ServiceDataType;
+  // languageLinks: LanguageLinks;
+};
+
+export default function Service({ service }: ServicePageProps) {
+  console.log(service);
+
+  return (
+    <div>
+      <pre>
+        <code>{JSON.stringify(service, null, 2)}</code>
+      </pre>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  const paths = await drupal.getStaticPathsFromContext(
+    "node--service",
+    context,
+  );
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+};
+export const getStaticProps: GetStaticProps = async (context) => {
+  try {
+    const path = await drupal.translatePathFromContext(context, {
+      pathPrefix: "/services",
+    });
+    if (!path) {
+      return {
+        notFound: true,
+      };
+    }
+    const service = await drupal.getResourceFromContext<DrupalNode>(
+      path,
+      context,
+      {
+        params: getNodePageJsonApiParams("node--service").getQueryObject(),
+      },
+    );
+
+    const valideService = ServiceSchema.parse(service);
+    // const nodeTranslations = await getNodeTranslatedVersions(
+    //   service,
+    //   context,
+    //   drupal,
+    // );
+    // const languageLinks = createLanguageLinks(nodeTranslations);
+    return {
+      props: {
+        ...(await getCommonPageProps(context)),
+        service: valideService,
+        // languageLinks,
+      },
+      revalidate: 60,
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+};
