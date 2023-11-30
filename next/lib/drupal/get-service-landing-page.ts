fd12db8383a2d7546a3251a971a1da2e96bbb150
Bishwajit Das <bishwajitkuat@gmail.com>
2023-11-30 14:24:09 +0200

    Featching data for service_landing_page content type
    getValidatedCleanServicesLandingPage function fetch data for service_landing_page content type from drupal. Filters added in params so that only necessary data will fetched from drual

---
+++ next/lib/drupal/get-service-landing-page.ts
@@ -0,0 +1,24 @@
+import { DrupalNode } from "next-drupal";
+import { drupal } from "./drupal-client";
+import { validateCleanupServiceData } from "../zod/services";
+
+export const getValidatedCleanServicesLandingPage = async (context: any) => {
+  const service_landing_page = await drupal.getResourceCollectionFromContext<
+    DrupalNode[]
+  >("node--service_landing_page", context, {
+    params: {
+      "filter[status]": 1,
+      "filter[langcode]": context.locale,
+      "fields[node--service_landing_page]":
+        "id,field_page_title,field_service_page_statement,field_wunder_working_strategy",
+      "fields[paragraph--wunder_working_strategy]":
+        "id,field_strategies,field_strategy_group_heading",
+      "fields[paragraph--heading_description]":
+        "id,field_heading_desc_heading,field_heading_desc_description",
+      include: "field_wunder_working_strategy.field_strategies",
+    },
+  });
+  // validation and clean up of fetched data
+
+  return service_landing_page;
+};
