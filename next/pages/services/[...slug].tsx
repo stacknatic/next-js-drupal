a73e051ed0a8b36e0c95c1900e9f5ed79003b641
Bishwajit Das <bishwajitkuat@gmail.com>
2023-12-05 13:47:40 +0200

    services and serviceCategories data fetching
    data fetched for all servies and serviceCategores from druapl and passed into ServiceSingle component, their types also added to ServicePageProps

--- next/pages/services/[...slug].tsx
+++ next/pages/services/[...slug].tsx
@@ -6,7 +6,10 @@ import {
 import { drupal } from "@/lib/drupal/drupal-client";
 import { getNodePageJsonApiParams } from "@/lib/drupal/get-node-page-json-api-params";
 import { getNodeTranslatedVersions } from "@/lib/drupal/get-node-translated-versions";
+import { getValidatedCleanServiceCategories } from "@/lib/drupal/get-service-catagories";
+import { getValidatedCleanServices } from "@/lib/drupal/get-services";
 import { getCommonPageProps } from "@/lib/get-common-page-props";
+import { ServiceCategoryType } from "@/lib/zod/service-categories";
 import { ServiceDataType, ServiceSchema } from "@/lib/zod/services";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { DrupalNode } from "next-drupal";
@@ -14,13 +17,25 @@ import React from "react";
 
 type ServicePageProps = {
   service: ServiceDataType;
+  services: ServiceDataType[];
+  serviceCategories: ServiceCategoryType[];
   // languageLinks: LanguageLinks;
 };
 
-export default function Service({ service }: ServicePageProps) {
-  console.log(service);
+export default function Service({
+  service,
+  services,
+  serviceCategories,
+}: ServicePageProps) {
+  
 
-  return <ServiceSingle service={service} />;
+  return (
+    <ServiceSingle
+      service={service}
+      services={services}
+      serviceCategories={serviceCategories}
+    />
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
@@ -58,10 +73,15 @@ export const getStaticProps: GetStaticProps = async (context) => {
     //   drupal,
     // );
     // const languageLinks = createLanguageLinks(nodeTranslations);
+    const validatedServices = await getValidatedCleanServices(context);
+    const validatedServiceCategories =
+      await getValidatedCleanServiceCategories(context);
     return {
       props: {
         ...(await getCommonPageProps(context)),
         service: valideService,
+        services: validatedServices,
+        serviceCategories: validatedServiceCategories,
         // languageLinks,
       },
       revalidate: 60,
