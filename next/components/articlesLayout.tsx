c6c8118d00a9ee5a4a4cf179172e0e8ca16456f8
Bishwajit Das <bishwajitkuat@gmail.com>
2023-12-19 23:14:41 +0200

    Removing articlesLayout route Thes component does not qualify as route and gives build time error. I believe mistakenly this file is being created inside of pages folder instead of component folder. So, removing from pages folder and putting inside component folder, in case it is needed later.

---
+++ next/components/articlesLayout.tsx
@@ -0,0 +1,21 @@
+// DynamicPageLayout.tsx
+import React, { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import ArticleSidebar from "@/components/articleSidebar";
+
+interface DynamicPageLayoutProps {
+  children: ReactNode;
+}
+
+const ArticleLayout: React.FC<DynamicPageLayoutProps> = ({ children }) => {
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="ml-1 p-1 main-content-container">
+        <>{children}</>
+      </div>
+    </>
+  );
+};
+
+export default ArticleLayout;
