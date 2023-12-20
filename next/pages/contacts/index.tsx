1a67543ea4981b9812c1d593330993a4b5522150
Bishwajit Das <bishwajitkuat@gmail.com>
2023-12-20 15:37:03 +0200

    Added title

--- next/pages/contacts/index.tsx
+++ next/pages/contacts/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import { useTranslation } from "next-i18next";
 import { getCommonPageProps } from "@/lib/get-common-page-props";
-import ContactUsPage from '@/components/contact-us-page/contact-us-page';
-import { getValidatedContactPersons } from '@/lib/drupal/get-contact-persons';
+import ContactUsPage from "@/components/contact-us-page/contact-us-page";
+import { getValidatedContactPersons } from "@/lib/drupal/get-contact-persons";
+import { Meta } from "@/components/meta";
 
 export default function Contact({ contactPersons }) {
   const { t } = useTranslation();
 
   return (
     <>
+      <Meta title={t("Contact")} />
       <ContactUsPage contactPersons={contactPersons} />
     </>
   );
@@ -19,7 +21,7 @@ export const getStaticProps = async (context) => {
   return {
     props: {
       ...(await getCommonPageProps(context)),
-      contactPersons
+      contactPersons,
     },
   };
 };
